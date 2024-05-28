ad512921450604ff8e4d9a3c1fd84fc384bf416d
Noelhy <69446757+Noelhy@users.noreply.github.com>
2024-05-27 20:23:26 -0600

    Create Noelhy.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Noelhy.py
@@ -0,0 +1,35 @@
+#00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+#  /* EJERCICIO:
+#  * - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+# https://www.python.org/
+#  * - Representa las diferentes sintaxis que existen de crear comentarios
+#  *   en el lenguaje (en una línea, varias...).
+
+#Comentario en una linea
+
+""" 
+Esto es un comentario en varias lineas con dobles comillas
+"""
+
+'''
+Comentario en linea con comillas simples
+'''
+
+#  * - Crea una variable (y una constante si el lenguaje lo soporta).
+var="mi primer variable"
+
+#  * - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+# Strings (str)
+palabra ="Strings (str)"
+print(palabra)
+# Integers (int).
+int_num=123
+print(int_num)
+# Floats (float).
+int_float=3.1416
+print(int_float)
+# Booleans (bool)
+verdad=True
+print(verdad)
+
+print("¡Hola, python!")
